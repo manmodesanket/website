89c3248e1b9525e608752074d911198297480176
Sanket Manmode <manmodesanket@gmail.com>
2022-01-14 07:54:26 +0530

    fix: work in progress

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -10,7 +10,7 @@ const Intro: React.FC<{}> = () => {
 
   return (
     <Layout>
-      <main className="min-h-screen flex flex-col justify-around py-16 md:py-32">
+      <main className="min-h-screen flex flex-col justify-around">
         <h1 className="text-3xl py-2">Hi there!👋</h1>
         <h2 className="text-3xl py-2">
           I am <span className="font-bold">Sanket Manmode</span>, a software
@@ -29,6 +29,9 @@ const Intro: React.FC<{}> = () => {
               </li>
             ))}
           </ul>
+        </section>
+        <section className="text-2xl">
+          <p>Work in progress 🚧</p>
         </section>
       </main>
     </Layout>
