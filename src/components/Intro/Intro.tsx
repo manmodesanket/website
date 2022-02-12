49695aeb97844fd9c3780b43b6a81bfccfaada93
Sanket Manmode <manmodesanket@gmail.com>
2022-02-12 13:51:58 +0530

    feat: Change in intro description

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -6,19 +6,56 @@ const Intro: React.FC<{}> = () => {
     twitter: "https://twitter.com/_manmodesanket",
     github: "https://github.com/manmodesanket",
     linkedin: "https://www.linkedin.com/in/manmodesanket/",
+    medium: "https://manmodesanket.medium.com/",
+    peerlist: "https://peerlist.io/sanket",
+    polywork: "https://www.polywork.com/manmodesanket",
   };
 
   return (
     <Layout>
       <main className="min-h-screen flex flex-col justify-around">
-        <h2 className="text-3xl py-2">Hi there!👋</h2>
-        <h2 className="text-3xl py-2">
-          I am <span className="font-bold">Sanket Manmode</span>, a software
-          enginner. Welcome to my space on internet. Currently I am focused on
-          learning how to build websites with great user experience and
-          performance. I work as a frontend engineer at Ridecell.
-        </h2>
-        <section className="text-2xl">
+        <section className="mb-4 md:mb-0">
+          <h2 className="text-3xl py-2">Hi there!👋</h2>
+        </section>
+        <section className="mb-8 md:mb-0">
+          <h2 className="text-3xl py-2">
+            I am <span className="font-bold">Sanket Manmode</span>, a software
+            enginner. Welcome to my space on internet. I am focused on learning
+            how to build websites with great user experience and performance. I
+            love JavaScript and I strive to know it better.
+          </h2>
+        </section>
+        <section className="text-2xl mb-8 md:mb-0">
+          <h2 className="text-3xl font-bold mb-4">Background</h2>
+          <p className="mb-4">
+            I am a Frontend Engineer at{" "}
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://ridecell.com/"
+              className="underline"
+            >
+              Ridecell
+            </a>{" "}
+            building things for the web with some awesome people. I am also a
+            graduate from{" "}
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://neog.camp/"
+              className="underline"
+            >
+              neogcamp
+            </a>{" "}
+            where I learnt how to build full stack web apps.
+          </p>
+          <p className="mb-4">
+            I like to write code which is easy to understand and easy to
+            maintain. I also try to make it efficient.
+          </p>
+          <p className="mb-4">My mantra is make things easy for others.</p>
+        </section>
+        <section className="text-2xl mb-8 md:mb-0">
           <p>You can find me at:</p>
           <ul className="flex space-x-2">
             {Object.keys(socials).map((item) => (
@@ -30,7 +67,7 @@ const Intro: React.FC<{}> = () => {
             ))}
           </ul>
         </section>
-        <section className="text-2xl">
+        <section className="text-2xl mb-8 md:mb-0">
           <p>Work in progress 🚧</p>
         </section>
       </main>
